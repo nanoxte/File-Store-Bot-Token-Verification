06875c1a65e67c9bbbdccc7428f367d10e32a9c1
nanoxte <166683877+nanoxte@users.noreply.github.com>
2024-05-12 16:06:48 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>Seeu</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/+qdLjzK5bWoViOWQ1'>CHANNEL</a>\n○ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/terao2'>SEEUTECH</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/+qdLjzK5bWoViOWQ1'>MAIN</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href=''>TERABOXhttps://t.me/terao2</a></b>",
+            text = f"<b>○ ᴏᴡɴᴇʀ : <a href='tg://user?id={OWNER_ID}'>Seeu</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/+qdLjzK5bWoViOWQ1'>CHANNEL</a>\n○ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/terao2'>SEEUTECH</a>\n○ ᴏᴜʀ ᴄᴏᴍᴍᴜɴɪᴛʏ : <a href='https://t.me/+qdLjzK5bWoViOWQ1'>MAIN</a>\n○ BOT : <a href='https://t.me/terao2'>TERABOX</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
